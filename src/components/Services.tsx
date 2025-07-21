76e35c6c4e4445c4b21355652a3d4fac31b3047f
Erik Hjelm Fjeldheim <erik@fjelldata.com>
2025-07-21 22:11:18 +0200

    Små forbedringer

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -21,7 +21,7 @@ const Services: React.FC = () => {
   ];
 
   return (
-    <div className="bg-black text-white py-20">
+    <section className="bg-black text-white py-20 min-h-screen">
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
           <h1 className="text-4xl lg:text-5xl font-bold mb-6">IT-tjenester for din bedrift</h1>
@@ -74,7 +74,7 @@ const Services: React.FC = () => {
           </a>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
